628289eefb0525f2a86e4fc885ed811758b2d78f
TobiasEmilHarbo <Steyr1990AUG>
2016-11-25 12:01:56 +0100

    accelerometer logging

--- app/src/main/java/com/example/tobias/contextawareapp/MainActivity.java
+++ app/src/main/java/com/example/tobias/contextawareapp/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.tobias.contextawareapp;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
+    final private String DEBUG_TAG = this.getClass().getSimpleName();
+    private SensorEventListener eventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        eventListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+
+                if (sensorEvent.sensor.getType() != Sensor.TYPE_ACCELEROMETER)
+                    return;
+
+                float sensorX = Math.abs(sensorEvent.values[0]);
+                float sensorY = Math.abs(sensorEvent.values[1]);
+                float sensorZ = Math.abs(sensorEvent.values[2]);
+
+                Log.d(DEBUG_TAG, sensorX + " : " + sensorY + " : " + sensorZ);
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        sensorManager.registerListener(eventListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        try {
+            sensorManager.unregisterListener(eventListener);
+        }catch (NullPointerException e) {} //ignore
     }
 }
